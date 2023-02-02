e70014ba51ddc2ab622c4cd324f7c75ed8cfca61
Masakitenchi <fucong119@gmail.com>
2023-02-02 12:37:55 +0900

    Update
    Fixed other implants are not working for Androids

---
+++ Source/AndroidBodyPartDefOf.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Androids
+{
+    [DefOf]
+    internal static class AndroidBodyPartDefOf
+    {
+        public static BodyPartDef AndroidThorax;
+        public static BodyPartDef ArtificialAndroidBrain;
+
+        static AndroidBodyPartDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(BodyPartGroupDefOf));
+        }
+    }
+    
+}
